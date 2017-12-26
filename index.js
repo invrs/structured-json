73ad28aa20478b92e779ad9bc02a21a495e9ead2
Winton Welsh <mail@wintoni.us>
2017-12-26 01:26:52 -0500

    mergeRefs test

--- index.js
+++ index.js
@@ -1,4 +1,64 @@
-function sum(a, b) {
-  return a + b;
+import { readFileSync } from "fs"
+
+export function build(path, options = {}) {
+  const json = readJson(path)
+
+  condition({ json, options })
+  reference({ json, options })
+  defaults({ json, options })
+  
+  return json
 }
-module.exports = sum;
+
+export function readJson(path) {
+  return JSON.parse(readFileSync(path, "utf8"))
+}
+
+export function condition({ json, options }) {
+  for (let key in json) {
+    if (json[key] && options[key]) {
+      Object.assign(json, json[key])
+      delete json[key]
+    } else if (json[key].constructor === Object) {
+      condition({ json: json[key], options })
+    }
+  }
+}
+
+export function reference({ json, ctx=json }) {
+  for (let key in ctx) {
+    let match = ctx[key].match(/^(\$|<<)/)
+    if (match) {
+      ctx[key] = mergeRefs({ json, ctx, key })
+    }
+    if (ctx[key].constructor === Object) {
+      reference({ json, ctx: ctx[key] })
+    }
+  }
+}
+
+export function mergeRefs({ json, ctx=json, key }) {
+  let vals = []
+  let regex = /(<<\s*)?\$([\w\.\-]+)/g
+  let match
+
+  if (json[key].match(/^<</)) {
+    json[key] = `$default.${key} ${json[key]}`
+  }
+
+  while (match = regex.exec(json[key])) {
+    let keys = match[2].split(/\./)
+    let obj = keys.reduce(
+      (memo, key, index) => {
+        let mixin = `$${key}`
+        let c = ctx[mixin] || ctx[key]
+        let m = memo[mixin] || memo[key]
+        return index == 0 ? c || m : m
+      },
+      json
+    )
+    vals.push(obj)
+  }
+
+  return vals
+}
--- index.test.js
+++ index.test.js
@@ -1,5 +1,26 @@
-const sum = require('./');
+import { build, mergeRefs, readJson } from "./index"
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+test('mergeRefs', () => {
+  let json = { "abc": "def", "ghi": "$abc" }
+  expect(mergeRefs({ json, key: "ghi" })).toEqual([ "def" ])
+
+  json = {
+    "abc": { "def": { "ghi": "jkl" } },
+    "mno": "$abc.def.ghi"
+  }
+  expect(mergeRefs({ json, key: "mno" })).toEqual([ "jkl" ])
+
+  json = {
+    "$default": { "abc": "def" },
+    "ghi": "$default.abc"
+  }
+  expect(mergeRefs({ json, key: "ghi" })).toEqual([ "def" ])
+
+  json = {
+    "$default": { "abc": { "def": "ghi" } },
+    "abc": "<< $jkl",
+    "jkl": { "mno": "pqr" }
+  }
+  expect(mergeRefs({ json, key: "abc" }))
+    .toEqual([ { "def": "ghi" }, { "mno": "pqr" } ])
+})
