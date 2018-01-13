ce19df13931400aca16b44ae1ca1638c29e9f45d
Winton Welsh <mail@wintoni.us>
2018-01-12 20:07:49 -0500

    All operations except dynamic getter creation

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,17 @@
-import { defineHiddenProps, mergeDefaults, resolveMixins, deleteMixins } from "../lib"
+import { readFileSync } from "fs"
+import {
+  build,
+  defineHiddenProps,
+  deleteMixins,
+  markDynamics,
+  mergeDefaults,
+  resolveMixins
+} from "../lib"
+
+const fixture = JSON.parse(
+  readFileSync(`${__dirname}/fixture.json`, "utf8")
+)
+
 
 function testObj(obj) {
   defineHiddenProps({ rootObj: obj, json: JSON.stringify(obj) })
@@ -6,49 +19,52 @@ function testObj(obj) {
 }
 
 test("defineHiddenProps", () => {
-  let obj = testObj({ "test": {} })
-  expect(obj._ops).toEqual([])
-  expect(obj._original).toEqual({ "test": {} })
+  let rootObj = testObj({ "test": {} })
+  expect(rootObj._ops).toEqual([])
+  expect(rootObj._original).toEqual({ "test": {} })
 })
 
 test("deleteMixins", () => {
-  let obj = testObj({
+  let rootObj = testObj({
     "$mixin1": {},
     "test": {
       "$mixin2": {}
     }
   })
-  deleteMixins({ rootObj: obj })
-  expect(obj._ops).toEqual([
+  deleteMixins({ rootObj, obj: rootObj._original })
+  expect(rootObj._ops).toEqual([
     { op: 'delete', loc: [ '$mixin1' ] },
     { op: 'delete', loc: [ 'test', '$mixin2' ] }
   ])
 })
 
 test("resolveMixins", () => {
-  let obj = testObj({
+  let rootObj = testObj({
     "$mixin1": {},
     "$mixin2": {},
     "test": {
       "$mixin2": {},
-      "testChild": "<= $mixin1 << $mixin2"
+      "testChild": "<= $mixin1 << $mixin2",
+      "<<? $mixin2": "$mixin1"
     }
   })
-  resolveMixins({ rootObj: obj })
-  expect(obj._ops).toEqual([
-    { op: 'resolve',
-      loc: [ 'test', 'testChild' ],
-      from: '$mixin1',
-      to: '$mixin1' },
-    { op: 'resolve',
-      loc: [ 'test', 'testChild' ],
-      from: '$mixin2',
-      to: 'test.$mixin2' }
+  resolveMixins({ rootObj, obj: rootObj._original })
+  expect(rootObj._ops).toEqual([
+    { "op": "replaceInValue",
+      "from": "$mixin2",
+      "to": "test.$mixin2",
+      "loc": ["test", "testChild"]
+    },
+    { "op": "replaceInKey",
+      "from": "$mixin2",
+      "to": "test.$mixin2",
+      "loc": ["test", "<<? $mixin2"]
+    }
   ])
 })
 
 test("mergeDefaults", () => {
-  let obj = testObj({
+  let rootObj = testObj({
     "condition": true,
     "<<": { "test": {} },
     ">>": { "test3": {} },
@@ -58,13 +74,46 @@ test("mergeDefaults", () => {
       "<<? condition condition2": { "conditional2": {} }
     }
   })
-  mergeDefaults({ rootObj: obj })
-  expect(obj).toEqual({
-    "condition": true,
-    "test": { "test3": { "test4": {} } },
-    "test2": {
-      "conditional": { "test4": {} },
-      "test3": { "test4": {} }
+  mergeDefaults({ rootObj, obj: rootObj._original })
+  expect(rootObj._ops).toEqual([
+    { op: 'mergeOver',
+      dest: [ 'test2' ],
+      source: [ 'test2', '<<? condition' ] },
+    { op: 'mergeOver',
+      dest: [],
+      source: [ '<<' ] },
+    { op: 'mergeUnder',
+      dest: [ 'test2' ],
+      source: [ '>>' ] },
+    { op: 'mergeUnder',
+      dest: [ '<<', 'test' ],
+      source: [ '>> >>' ] },
+    { op: 'mergeUnder',
+      dest: [ '>>', 'test3' ],
+      source: [ '>> >>' ] },
+    { op: 'mergeUnder',
+      dest: [ '>> >>', 'test4' ],
+      source: [ '>> >>' ] }
+  ])
+})
+
+test("mergeDefaults", () => {
+  let rootObj = testObj({
+    "test": "1",
+    "$mixin": {},
+    "<<": { "test2": "2" },
+    "test3": {
+      "test4": "<= $mixin"
     }
   })
+  markDynamics({ rootObj, obj: rootObj._original })
+  expect(rootObj._ops).toEqual([
+    { op: 'isDynamic', loc: [ 'test3', 'test4' ] }
+  ])
+})
+
+test("build", () => {
+  let rootObj = build(fixture, { staging: true })
+  console.log(rootObj._ops)
+  console.log(rootObj)
 })
