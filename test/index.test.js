b998df020ca07dc687a78d167f12d5b85399a5ea
Winton Welsh <mail@wintoni.us>
2018-01-15 21:28:25 -0500

    Fixture test

--- test/index.test.js
+++ test/index.test.js
@@ -123,5 +123,35 @@ test("mergeDefaults", () => {
 
 test("build", () => {
   let base = build(fixture, { staging: true })
-  console.log(JSON.stringify(base, null, 2))
+  expect(base).toEqual({
+    "aws-account": {
+      "east": {
+        "image-bucket": {
+          "hello": false,
+          "world": true
+        },
+        "basic-bucket": {
+          "hello": true,
+          "world": true
+        }
+      },
+      "west": {
+        "image-bucket": {
+          "accelerate": true,
+          "error": "error.html"
+        }
+      }
+    },
+    "aws-bucket": {
+      "image": {
+        "index2": "index.html",
+        "index3": "index.html",
+        "hello": false,
+        "name": "company-images-stag",
+        "test": true,
+        "grant": "id=xxx"
+      }
+    },
+    "staging": true
+  })
 })
