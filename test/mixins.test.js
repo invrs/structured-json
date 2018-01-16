ec461f763918f23b8a8a14aed4e1bfed1fa56178
Winton Welsh <mail@wintoni.us>
2018-01-15 22:47:22 -0500

    Allow default references

---
+++ test/mixins.test.js
@@ -0,0 +1,23 @@
+import { build } from "../lib"
+
+test("mixins", () => {
+  let { products } = build({
+    "products": {
+      "$green": {
+        "color": "green"
+      },
+      "$white": {
+        "color": "white"
+      },
+      "milk": { "<<": "<= $white" },
+      "kale": { "<<": "<= $green" },
+      "tofu": { "<<": "<= $white" },
+    }
+  })
+
+  expect(products).toEqual({
+    milk: { color: 'white' },
+    kale: { color: 'green' },
+    tofu: { color: 'white' },
+  })
+})
